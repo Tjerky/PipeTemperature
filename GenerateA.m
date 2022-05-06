f48aec82b2d38620761ab149a01583320601a79c
20213382 <20213382@S20213382.campus.tue.nl>
2022-05-06 14:56:02 +0200

    Wrote a complete version for GenerateA.m

--- GenerateA.m
+++ GenerateA.m
@@ -3,12 +3,22 @@
 % function definition
 function A = GenerateA(N, alpha, beta)
     % generate a NxN matrix filled with zeros
+    A = zeros(N, N);
     
     % fill middle rows
+    for j = 2:N-1
+        % 1-alpha on the diagonal
+        A(j, j) = 1-alpha;
+        % alpha on the left diagonal
+        A(j, j-1) = alpha;
+        % alpha on the right diagonal
+        A(j, j+1) = alpha;
     
     % fill upper row
+    A(1,1) = 2+alpha-2*beta;
+    A(1,2) = alpha;
     
     % fill lower row
-    
-    A = 0;
+    A(N,N) = 2-alpha;
+    A(N, N-1) = alpha;
 end
