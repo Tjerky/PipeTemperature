2e633c03c96695fd2342cdd6d063d2aa47fb39cd
20213382 <20213382@S20213382.campus.tue.nl>
2022-05-06 13:54:00 +0200

    Setup of the git repository and the main file

---
+++ main.m
@@ -0,0 +1,15 @@
+%% Main file modelling the temperature distribution of a pipe
+% define the constants
+
+% define the matrix
+
+% initial conditions
+
+% loop
+    % solve matrix equation for the temperature inside the pipe
+    
+    % derive the new temperture of the water
+    
+    % store the new timestep in array
+    
+% store the simulation in as a file
