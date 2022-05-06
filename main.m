e7b0717253b75c7b0c65d34d451e8f0099c4fb34
20213382 <20213382@S20213382.campus.tue.nl>
2022-05-06 14:20:56 +0200

    Added the dimensions and material properties for the polyurethane tube.

--- main.m
+++ main.m
@@ -1,7 +1,20 @@
 %% Main file modelling the temperature distribution of a pipe
 % define the constants
+    % dimensions
+R = 0.006; % m, outside radius of the tube
+d = 0.001; % m, thickness of the tube wall
+N = 100; % number of gridpoints in the r direction
+M = 100; % number of gridpoints in the theta direction
+    
+    % material properties
+rho = 295.5; % kg/m^3, density of the pipe material
+k = 0.13; % W/(m K), thermal conductivity of the pipe material
+cp = 1470; % J/(kg K), specific heat of the pipe material
+c = 5000; % W/(m^2 K), heat transfer coefficient pipe -> water
 
 % define the matrix
+alpha = k/(rho*cp); % constant to simplify the formulas
+beta = c/(rho*cp); % constant to simplify the formulas
 
 % initial conditions
 
