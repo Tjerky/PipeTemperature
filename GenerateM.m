bcf9dd67e6489faa6349c643519ee2bf35c53282
20213382 <20213382@S20213382.campus.tue.nl>
2022-05-06 14:38:00 +0200

    Wrote a complete version for GenerateM.m

--- GenerateM.m
+++ GenerateM.m
@@ -1,13 +1,24 @@
 %% Function to generate the M matrix in the matrix equation
 
 %function definition
-function M = GenerateM(N, alpha, beta)
-    % generate 0 matrix
+function M = GenerateM(N, alpha)
+    % generate a NxN matrix filled with zeros
+    M = zeros(N, N);
     
     % fill middle rows
+    for j = 2:N-1
+        % 2 + 2 * alpha on the diagonal
+        M(j, j) = 2*alpha + 2;
+        % -alpha on the left diagonal
+        M(j, j-1) = -1*alpha;
+        % -alpha on the right diagonal
+        M(j, j+1) = -1*alpha;
+    end
     
     %fill upper row
-    
+    M(1, 1) = 2+alpha;
+    M(1, 2) = -1*alpha;
     % fill lower row
-    
-    M = 0;
+    M(N,N) = 2+alpha;
+    M(N, N-1) = -1*alpha;
+end
