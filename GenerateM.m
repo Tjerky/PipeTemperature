a7e227451baa0000467d8b669007d7bdfc6220a9
20213382 <20213382@S20213382.campus.tue.nl>
2022-05-06 14:28:21 +0200

    Setup the GenerateM file.

---
+++ GenerateM.m
@@ -0,0 +1,13 @@
+%% Function to generate the M matrix in the matrix equation
+
+%function definition
+function M = GenerateM(N, alpha, beta)
+    % generate 0 matrix
+    
+    % fill middle rows
+    
+    %fill upper row
+    
+    % fill lower row
+    
+    M = 0;
