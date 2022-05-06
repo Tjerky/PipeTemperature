5b1a57d2fd28521bfc8108af860cee5495fbfac3
20213382 <20213382@S20213382.campus.tue.nl>
2022-05-06 14:46:18 +0200

    Setup the GenerateA file

---
+++ GenerateA.m
@@ -0,0 +1,14 @@
+%% Function to generate the A matrix in the matrix equation
+
+% function definition
+function A = GenerateA(N, alpha, beta)
+    % generate a NxN matrix filled with zeros
+    
+    % fill middle rows
+    
+    % fill upper row
+    
+    % fill lower row
+    
+    A = 0;
+end
